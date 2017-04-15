b82b34b0fcff0157666ea1f699b534e1ad6ba6db
Rafael Xavier de Souza <rxaviers@gmail.com>
2017-04-15 15:36:09 -0300

    Fix indentation

--- test/zoned-date-time.js
+++ test/zoned-date-time.js
@@ -13,43 +13,43 @@ describe("zoned-date-time", function() {
     expect(new ZonedDateTime(new Date("2015-06-13T01:02:03Z"), zoneData.Asia.Dubai)).to.equalZonedDateTime(
       [2015, 6, 13, 5, 2, 3],
       {isDST: false}
-   );
+    );
 
     // Testing DST edge cases...
     // BRST (daylight savings)
     expect(new ZonedDateTime(new Date("2017-02-19T01:00:00Z"), zoneData.America.Sao_Paulo)).to.equalZonedDateTime(
       [2017, 2, 18, 23, 0, 0],
       {isDST: true}
-   );
+    );
 
     // BRT
     expect(new ZonedDateTime(new Date("2017-02-19T02:00:00Z"), zoneData.America.Sao_Paulo)).to.equalZonedDateTime(
       [2017, 2, 18, 23, 0, 0],
       {isDST: false}
-   );
+    );
 
     // PST
     expect(new ZonedDateTime(new Date("2017-03-12T09:00:00Z"), zoneData.America.Los_Angeles)).to.equalZonedDateTime(
       [2017, 3, 12, 1, 0, 0],
       {isDST: false}
-   );
+    );
 
     // PDT (daylight savings)
     expect(new ZonedDateTime(new Date("2017-03-12T10:00:00Z"), zoneData.America.Los_Angeles)).to.equalZonedDateTime(
       [2017, 3, 12, 3, 0, 0],
       {isDST: true}
-   );
+    );
 
     // ARST (it's an adhoc daylight savings using -03 offset)
     expect(new ZonedDateTime(new Date(938919600000), zoneData.America.Argentina.Buenos_Aires)).to.equalZonedDateTime(
       [1999, 10, 3, 0, 0, 0],
       {isDST: true}
-   );
+    );
 
     // ART
     expect(new ZonedDateTime(new Date(952052400000), zoneData.America.Argentina.Buenos_Aires)).to.equalZonedDateTime(
       [2000, 3, 3, 0, 0, 0],
       {isDST: false}
-   );
- });
+    );
+  });
 });
